cb95244ba040b8020a2bcac7d96a0c5c347471a0
riastudygogo <riastudygogo@gmail.com>
2025-12-04 01:47:53 +0800

    更新 PWA 支援，調整 manifest 檔案並優化 service worker 快取

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener("install", () => {
-  console.log("Service Worker installed.");
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("dog-age-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./css/style.css",
+        "./js/script.js",
+        "./img/corgi.png",
+        "./manifest.json"
+      ]);
+    })
+  );
+  console.log("SW installed & cached.");
 });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker activated.");
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
